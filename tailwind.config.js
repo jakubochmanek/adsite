b7d31c64a489f94d551dde28310f81eca628c5f9
jakubochmanek <jakubochmanek@gmail.com>
2024-09-03 21:50:23 +0200

    80% layout completed: header, main, section, footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: "#0147FF",
+        customGrey: "#282828",
+      },
+      width: {
+        "1440px": "1440px",
+        "600px": "600px",
+      },
+      height: {
+        mainHeight: "825px",
+        mainContainerHeight: "343px",
+        "446px": "446px",
+      },
+      padding: {
+        "120px": "120px",
+      },
+      gap: {
+        "48px": "48px",
+      },
+      fontFamily: {
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
